7d4e38459678ff9517b2b95ac1136ff4ee93faa4
Vigilantee-seek <aaronxia@foxmail.com>
2018-06-12 21:31:12 +0800

    removing some redundant code, add Point3d class and reflect API

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <GL/glut.h>
 #include <vector>
-#include <math.h>
+
 #define ITER 1
 
 const GLint windowLeft = 0, windowTop = 0;
 const GLint winWidth = 800, winHeight = 600;
 const GLint wid = winWidth / (2 * ITER);
 const GLint wih = winHeight / (2 * ITER);
-GLdouble Mt[4][4]{ 0 };
 
 class Point {
-private:
+protected:
 	GLint fx;
 	GLint fy;
 public:
@@ -35,11 +34,39 @@ public:
 		else if (vec_size == 2) {
 			pts[0] = fx; pts[1] = fy;
 			return true;
-		} 
+		}
 		return false;
 	}
 }cp, old_mp, mp, fp;
 // Global variable for the "fixed" point, center point and motion point
+
+class Point3d :public Point {
+private:
+	GLint fz;
+public:
+	Point3d() :Point(){
+		fz = 0;
+	}
+
+	// The pts contains three elements: fx, fy and fz
+	void setPoint(GLint x = 0, GLint y = 0, GLint z = 0) {
+		fx = x;	fy = y; fz = z;
+	}
+
+	// Return the co-ordinates of "this" point
+	bool getPoint(std::vector<GLint>& pts) {
+		int vec_size = pts.size();
+		if (!vec_size) {
+			pts.push_back(fx); pts.push_back(fy); pts.push_back(fz);
+			return true;
+		}
+		else if (vec_size == 3) {
+			pts[0] = fx; pts[1] = fy;	pts[2] = fz;
+			return true;
+		}
+		return false;
+	}
+}lp;
 
 void init() {
 
@@ -62,18 +89,11 @@ void rotate3d(Point fixedp, Point oldmp,Point motionp, Point centerp) {
 	centerp.getPoint(cpp);
 	oldmp.getPoint(opp);
 
-	GLint ax = fpp[0] - cpp[0];
-	GLint ay = fpp[1] - cpp[1];
-
-	GLdouble angle;
-
 	GLint x = mpp[0] - opp[0];	GLint y = mpp[1] - opp[1];
 
 	// The object should rotate angleY around Y axis and angleX around X axis
 	GLdouble angleY = (x % wid) / GLdouble(wid) * 360;
 	GLdouble angleX = -(y % wih) / GLdouble(wih) * 360;
-
-	std::cout << angleY << ' ' << angleX << std::endl;
 
 	glRotated(angleX, 1.0, 0, 0);
 	glRotated(angleY, 0, 1.0, 0);
@@ -107,10 +127,13 @@ void onMotion(int x, int y) {
 	glutPostRedisplay();
 }
 
+// reflect light and we assume the z-coordinate of the center point is 0
+void reflect(Point3d lightp, Point centerp) {
+
+}
+
 int main(int argc, char* argv[]) {
 	const char* winName = "Rotating Box";
-	// Initialize Mt
-	Mt[3][3] = 1;
 	cp.setPoint(winWidth / 2, winHeight / 2);
 
 	glutInit(&argc, argv);
